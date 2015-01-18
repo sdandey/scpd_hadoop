c743230160a329199652ea591c6a93d4b9023757
sdandey <dd.santosh@gmail.com>
2015-01-18 01:28:16 -0500

    Added FindFrequentItems Program and PeopleYouMightKnow

---
+++ scpd_hadoop/src/main/java/com/scpd/cs246/hw1/FindFrequentItems.java
@@ -0,0 +1,110 @@
+package com.scpd.cs246.hw1;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.apache.commons.io.FileUtils;
+
+public class FindFrequentItems {
+
+	
+	public static void main(String[] args) throws IOException {
+		
+		List<String> baskets = FileUtils.readLines(new File(args[0]));
+		HashMap<String,Integer> mapSupportedItems = filterFrequentItemsBySupportThreshold(getItemCounts(baskets));
+		HashMap<String, Integer> supportedTubles =  filterFrequentItemsBySupportThreshold(generateItemPairs(baskets, 
+				toList(mapSupportedItems.keySet())));
+		
+		Set<String> keys = supportedTubles.keySet();
+		
+		for (String key : keys) {
+			System.out.println(key + "->" + supportedTubles.get(key));
+		}
+		
+	}
+	
+	
+	public static HashMap<String, Integer> generateItemPairs(List<String> baskets, List<String> frequentItems){
+		
+		HashMap<String, Integer>  mapItemPairs = new HashMap<String, Integer>();
+		
+		List<String> filteredItems;
+		
+		String pair;
+		for (String basketLine : baskets) {
+			filteredItems=filterFrequentItems(basketLine.split("\\s+"), frequentItems);
+			if(filteredItems.size() >=2){
+				for(int i = 0; i< filteredItems.size(); i++){
+					for(int j=i;j<filteredItems.size();j++){
+						
+						pair = i+","+j;
+						
+						if(mapItemPairs.containsKey(pair))
+							mapItemPairs.put(pair, mapItemPairs.get(pair) + 1);
+						else
+							mapItemPairs.put(i+","+j, 1);
+					}
+				}
+			}
+		}
+		return mapItemPairs;
+	}
+	
+	public static List<String> filterFrequentItems(String[] itemsinBasket, List<String> frequentItems){
+		
+		List<String> filteredItemsInBasket = new ArrayList<String>();
+				
+		for (String basketItem : itemsinBasket) 
+			if(frequentItems.contains(basketItem))
+				filteredItemsInBasket.add(basketItem);
+		
+		return filteredItemsInBasket;
+	}
+	
+	public static List<String> toList(Set<String> itemName){
+		List<String> items = new ArrayList<String>();
+		
+		for (String string : itemName) {
+			items.add(string);
+		}
+		return items;
+	}
+	
+	public static HashMap<String, Integer>  filterFrequentItemsBySupportThreshold(HashMap<String, Integer>
+		mapItemsByCount){
+		
+		HashMap<String,Integer> filteredMap = new HashMap<String, Integer>();
+		
+		Set<Entry<String, Integer>> entrySets =  mapItemsByCount.entrySet();
+		for (Entry<String, Integer> entry : entrySets) {
+			if(entry.getValue() >= 100)
+				filteredMap.put(entry.getKey(), entry.getValue());
+		}
+		return filteredMap;
+	}
+	
+	public static HashMap<String, Integer> getItemCounts(List<String> baskets){
+		
+		HashMap<String, Integer>  mapItems = new HashMap<String, Integer>();
+		
+		String[] items;
+		
+		for (String basket : baskets) {
+			items = basket.split("\\s+");
+			for (String item : items) {
+				if(mapItems.containsKey(item)){
+					mapItems.put(item, mapItems.get(item) + 1);
+				}
+				else
+					mapItems.put(item, 1);
+			}
+		}
+		return mapItems;
+	}
+	
+}
--- scpd_hadoop/src/main/java/com/scpd/cs246/hw1/PeopleYouMightKnow.java
+++ scpd_hadoop/src/main/java/com/scpd/cs246/hw1/PeopleYouMightKnow.java
@@ -1,141 +1,135 @@
 package com.scpd.cs246.hw1;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URI;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
-import org.apache.commons.io.FileUtils;
+import java.util.Map.Entry;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class PeopleYouMightKnow {
 
 	public static class Map
-	extends Mapper<Object, Text, Text, Text>{
+	extends Mapper<LongWritable, Text, IntWritable, Text>{
 
+		
+		/**
+		 * The mapper class provides an output of below for each userID
+		 *  list of (1 - friends)
+		 *  list of (2 - potential friends)
+		 */
 
-		public void map(Object key, Text value, Context context
+		public void map(LongWritable key, Text value, Context context
 				) throws IOException, InterruptedException {
 
 			//value will be of format of <USER> <TAB>  <FRIENDS LIST, delimited>
-
-			String[] userAndFriends = null;
-
-			if(value != null && !"".equals(value.toString().trim()))
-				userAndFriends = value.toString().split("\\s+");
+			String[] userAndFriends = value.toString().split("\t");
 
 			if(userAndFriends.length != 2)
 				return;
+			//String userId = userAndFriends[0];
+			String[] friends = userAndFriends[1].split(",");
 			
-			String userId = userAndFriends[0];
-			String friends = userAndFriends[1];
-
-			//load all the friends list for the user into the List
-			List<String> friendsList = new ArrayList<String>(Arrays.asList(friends.split(",")));
-
-			
-			//Load the entire document into a hashmap with userID as a key and List of Friends as value
-			
-			if(context.getCacheFiles() == null || context.getCacheFiles().length != 1){
-				context.write(new Text(userId), new Text("CACHE FILE NOT FOUND"));
-				return;
-			}
-
-			URI fileURI = context.getCacheFiles()[0];
-			HashMap<String, List<String>> mapCachedDocument = Utils.getCacheFileAsHashMap(FileUtils.readLines(new File("/Users/santoshdandey/Desktop/Dropbox/stanford/cache.txt")));
-			//System.out.println("mapCachedDocument:" + mapCachedDocument.toString());
-			
-			//now perform analysis on each friend and output friendId, number of mutual friends,  mutualFriendsList, potentialMutualFriends
-			
-			UserIntersectionFriend userIntersectionFriend = null;
-			
-			List<UserIntersectionFriend> listFriends = new ArrayList<UserIntersectionFriend>();
-		
-			for (String friendId : friendsList) {
-				userIntersectionFriend = Utils.getMutualFriend(friendsList, friendId, mapCachedDocument.get(friendId));
-				listFriends.add(userIntersectionFriend);
-//				System.out.println("userId:" + userId + "values " + userIntersectionFriend.toString());
-//				context.write(new Text(userId), new Text(userIntersectionFriend.toString()));
-			}
-
-			
-			//Sort the list based on the mutual friends count
-			Collections.sort(listFriends,new Comparator<UserIntersectionFriend>() {
-
-				public int compare(UserIntersectionFriend o1,
-						UserIntersectionFriend o2) {
-					return o1.getMutualFriendsCount() - o2.getMutualFriendsCount();
+			for (int i = 0; i < friends.length; i ++) {
+				//suppose A->B,C,D,E Then (A,B,1) (A,C,1) (A,D,1) (A,E,1) are first degree friends
+				for(int j=i+1; j< friends.length; j++){
+					//Suppose A->B,C,D,E  B can be friends with c (B,C,2) and c can be friends with d (C,B,2)
+					context.write(new IntWritable(Integer.valueOf(friends[i])), new Text(friends[j]));
+					context.write(new IntWritable(Integer.valueOf(friends[j])), new Text(friends[i]));
 				}
-			});
-			
-			String  mutualFriendsSuggestions = "";
-			
-			
-			int n = 0;
-			for (UserIntersectionFriend userIntersectionFriend2 : listFriends) {
-				
-				List<String> potentialFriendsList = userIntersectionFriend2.getPotentialFriends();
-				
-				if(n == 10)
-					break;
-				
-				for (String potentialFriend : potentialFriendsList) {
-					
-					if(n == 10)
-						break;
-					
-					mutualFriendsSuggestions += potentialFriend + "(" + userIntersectionFriend2.getMutualFriendsCount() + ")     " ;
-					n++;
-				}
-				
 			}
-			
-			context.write(new Text(userId), new Text(mutualFriendsSuggestions));
 		}
 	}
 	
+	public static class Reduce extends Reducer<IntWritable,Text,IntWritable,Text> {
 
-	public static class Reduce extends Reducer<Text,Text,Text,Text> {
-
-		public void reduce(Text userId, Iterable<Text> mapperOutputWithMutualFriendsInfo,
+		public void reduce(IntWritable userId, Iterable<Text> potentialFriends,
 				Context context
 				) throws IOException, InterruptedException {
 			
-			for (Text mapperIntersectionOutput : mapperOutputWithMutualFriendsInfo){
-				context.write(userId, new Text(mapperIntersectionOutput));
+			
+			HashMap<String, Integer> potentialFriendsCounts = new HashMap<String, Integer>();
+			
+			for (Text text : potentialFriends) {
 				
-				
+				if(potentialFriendsCounts.containsKey(text.toString())){
+					int count = potentialFriendsCounts.get(text.toString());
+					potentialFriendsCounts.put(text.toString(), count + 1);
+				}
+				else
+					potentialFriendsCounts.put(text.toString(), 1);
 			}
+		
+			 ArrayList<Entry<String, Integer>> potentialFriendsByHighesCount = new ArrayList<Entry<String, Integer>>();
+	            for (Entry<String, Integer> entry : potentialFriendsCounts.entrySet()) {
+	            	potentialFriendsByHighesCount.add(entry);
+	            }
+
+			 Collections.sort(potentialFriendsByHighesCount, new Comparator<Entry<String, Integer>>() {
+	                public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+	                    return o2.getValue().compareTo(o1.getValue());
+	                }
+	            });
+			
+			 
+			 String friendSuggestions = "\t";
+			 
+			 int n = 0;
+			 for (Entry<String, Integer> entry : potentialFriendsByHighesCount) {
+				 if(n<10)
+					 friendSuggestions += entry.getKey() + "(" + entry.getValue() + ")" + ",";
+				 else
+					 break;
+				 
+				 n++;
+			}
+			 
+			 String usersText = "924,8941,8942,9019,9020,9021,9022,9990,9992,9993";
+             
+			 String[] users = usersText.split(",");
+			 for (String user : users) {
+				if(user.equals(userId.toString()))
+					context.write(userId, new Text(friendSuggestions));
+			 }
 		}
+		
 	}
+	
 
 	public static void main(String[] args) throws Exception {
-
+       
+		
 		Configuration conf = new Configuration();
+        
+        Job job = Job.getInstance(conf, "Find Friends");
+        job.setJarByClass(PeopleYouMightKnow.class);
+        job.setOutputKeyClass(IntWritable.class);
+        job.setOutputValueClass(Text.class);
+ 
+        job.setMapperClass(Map.class);
+        job.setReducerClass(Reduce.class);
+ 
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+ 
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+ 
+        job.waitForCompletion(true);
+    
 
-		Job job = Job.getInstance(conf, "word count");
-
-		job.setJarByClass(PeopleYouMightKnow.class);
-		job.setMapperClass(Map.class);
-		job.setCombinerClass(Reduce.class);
-		job.setReducerClass(Reduce.class);
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Text.class);
-		job.addCacheFile(new URI("/Users/santoshdandey/Desktop/Dropbox/stanford/cache.txt"));
-
-		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 }
